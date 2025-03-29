2e0bb4bdfe792a529793b33d4e83967bdcd24cfb
Francesco Donnini <donnini.francesco00@gmail.com>
2025-03-29 23:46:14 +0100

    aggiunto stack con cas

--- stack/stack_cas.c
+++ stack/stack_cas.c
@@ -1,12 +1,24 @@
 #include "stack.h"
+#include <stdatomic.h>
+#include <stdbool.h>
 
 int stack_init(Stack *s) {
     return 0;
 }
 
 ListHead *stack_pop(Stack *s) {
+    bool b = true;
+    do {
+        ListHead *old_head = s->head.next;
+        b = atomic_compare_exchange_strong(&s->head.next, old_head, old_head->next);
+    } while (!b);
     return NULL;
 }
 
 void stack_push(Stack *s, ListHead *item) {
+    bool b = true;
+    do {
+        ListHead *old_head = s->head.next;
+        b = atomic_compare_exchange_strong(&s->head.next, old_head, item);
+    } while (!b);
 }
